d643ce89685fc0a52a7d076f2e2fba7bbd7e183d
LenninHuaman <u20201b975@upc.edu.pe>
2022-09-27 19:30:08 -0500

    Chore: Created files to start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,47 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatTableModule } from "@angular/material/table";
+import { MatPaginatorModule} from "@angular/material/paginator";
+import { MatInputModule} from "@angular/material/input";
+import { MatButtonModule} from "@angular/material/button";
+import { MatToolbarModule} from "@angular/material/toolbar";
+import { MatIconModule} from "@angular/material/icon";
+import { MatSortModule} from "@angular/material/sort";
+
+import { RegisterComponent } from './public/register/register.component';
+import { DeveloperComponent } from './public/register/developer/developer.component';
+import { DevelopersComponent } from './developers/developers.component';
+import { CompaniesComponent } from './companies/companies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    DeveloperComponent,
+    DevelopersComponent,
+    CompaniesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule, ReactiveFormsModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatInputModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
